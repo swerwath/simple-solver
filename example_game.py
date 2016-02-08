e674f6553303e107361d8067e15017c0254df2de
Scott Werwath <swerwath@berkeley.edu>
2016-02-07 22:12:38 -0800

    Deleted seperate file for example game

--- example_game.py
+++
@@ -1,9 +0,0 @@
-# We'll use the 4 game as our example
-class ExampleGame:
-	initial_position = 4
-	def primitive(pos):
-		return solver.LOSS if pos == 0 else solver.OTHER
-	def gen_moves(pos):
-		return [-1] if pos == 1 else [-1, -2]
-	def do_move(pos, move):
-		return pos + move
